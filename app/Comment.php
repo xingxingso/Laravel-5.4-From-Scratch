22967fbd7cdd91189c81a642aa5891a57ee7fb3a
Kant Chan <1126973789@qq.com>
2018-12-21 13:36:26 +0800

    Associating With Users

--- app/Comment.php
+++ app/Comment.php
@@ -10,4 +10,9 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
+
+    public function user() // $comment->user->name
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -13,6 +13,11 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    public function user() // $post->user  $comment->post->user
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function addComment($body)
     {
         $this->comments()->create(compact('body'));
