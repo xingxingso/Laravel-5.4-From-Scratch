719318744aa66a38f35126265f6ec0af56159714
Kant Chan <1126973789@qq.com>
2018-12-21 10:45:51 +0800

    addComment, use this->comments()->create()

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Illuminate\Http\Request;
+use App\Post;
+use App\Comment;
+
+class CommentsController extends Controller
+{
+    public function store(Post $post)
+    {
+        $this->validate(request(), ['body' => 'required|min:2']);
+
+        $post->addComment(request('body'));
+
+        // // add a comment to a post
+        // Comment::create([
+        //     'body'    => request('body'),
+        //     'post_id' => $post->id
+        // ]);
+
+        return back();
+    }
+}
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -21,5 +21,27 @@
                 @endforeach
             </ul>
         </div>
+        
+        {{-- Add a comment --}}
+        <hr>
+
+        <div class="card">
+            <div class="card-block">
+                <form method="POST" action="/posts/{{ $post->id }}/comments">
+                    {{-- {{ method_field('PATCH') }} --}}
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your comment here." class="form-control" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+                    </div>
+                </form>
+
+                @include ('layouts.errors')
+
+            </div>
+        </div>
     </div>
 @endsection
