b69ea271f536b4b3d46b16e6b82c456aa9cb266c
Kant Chan <1126973789@qq.com>
2018-12-24 10:25:05 +0800

    View Composers:   Very quickly, you'll encounter the situation when multiple views require the same piece of data. For example, if a sidebar widget requires a particular variable for every single page, well we can't be expected to pass that data from every single controller. No, instead, we can leverage view composers.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -34,14 +34,17 @@ class PostsController extends Controller
             ->filter(request(['month', 'year']))
             ->get();
 
-        // Temporary.
-        $archives = Post::selectRaw('year(created_at) year,monthname(created_at) month,count(*) published')
-            ->groupBy('year', 'month')
-            ->orderByRaw('min(created_at) desc')
-            ->get()
-            ->toArray();
+        // // Temporary.
+        // $archives = Post::selectRaw('year(created_at) year,monthname(created_at) month,count(*) published')
+        //     ->groupBy('year', 'month')
+        //     ->orderByRaw('min(created_at) desc')
+        //     ->get()
+        //     ->toArray();
 
-        return view('posts.index', compact('posts', 'archives'));
+        // $archives = Post::archives();
+
+        // return view('posts.index', compact('posts', 'archives'));
+        return view('posts.index', compact('posts'));
     }
 
     // public function show($id)
